3993f4c72f9933cf76629820e6e24f33a46d18a4
Daniel Nasr <danialnasronline@gmail.com>
2022-08-16 14:33:47 +0430

    Add Custom Length

--- main.py
+++ main.py
@@ -4,20 +4,27 @@ import pyperclip
 from colorama import Fore,Back, Style
 q = input("Read Password (Y/N): ").lower()
 if q == "y":
-     f = open("PASSWORDS", "r")
+     f = open("./PASSWORDS", "r")
      print(Back.WHITE + Fore.BLACK + f.read())
      print(Style.RESET_ALL)
      f.close()
 else:
+     try:
+               length = int(input("Password Length: "))
+     except:
+               print(Fore.RED + "Just Number")
+               print(Style.RESET_ALL)
+               exit
      lowercase = string.ascii_lowercase
      uppercase = string.ascii_uppercase
-     numbers = string.digits
+     digits = string.digits
+     punctuation = string.punctuation
      i = 0
      password = ""
-     arr = lowercase + uppercase + numbers
-     while i < 20:
+     arr = lowercase + uppercase + digits + punctuation
+     while i < length:
           password += random.choice(arr)
-          i+= 1
+          i += 1
 
      print(Fore.GREEN + password)
      print(Style.RESET_ALL)
@@ -33,6 +40,6 @@ else:
           answer = input("What Is Your Platform: ")
           if answer == "":
                answer = "No Platform"
-          file = open("PASSWORDS","a")
+          file = open("./PASSWORDS","a")
           file.write("\n\n"+answer + ": \n"+password)
           file.close()
